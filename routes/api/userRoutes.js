639591cd9865e84fb4045cf228277683778428c1
Jordan Newhouse <jkimys2@gmail.com>
2024-03-30 12:58:22 -0400

    user routes update

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,26 @@
+// Import files
+const router = require("express").Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  createFriend,
+  deleteFriend,
+} = require("../../controllers/userController");
+
+// Route /api/users
+router.route("/").get(getUsers).post(createUser);
+
+// Route /api/users/:userId
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// Route /api/users/:userId/friends/:friendId
+router
+  .route("/:userId/friends/:friendID")
+  .post(createFriend)
+  .delete(deleteFriend);
+
+// Export router
+module.exports = router;
