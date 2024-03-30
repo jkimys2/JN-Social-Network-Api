8910f9be3d404b040b2e55737adf8f40da487b0e
Jordan Newhouse <jkimys2@gmail.com>
2024-03-30 13:05:02 -0400

    thought routes update

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,27 @@
+// Import files
+const router = require("express").Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
+} = require("../../controllers/thoughtController");
+
+// Route /api/thoughts
+router.route("/").get(getThoughts).post(createThought);
+
+// Route /api/thoughts/:thoughtId
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// Route /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(createReaction);
+
+// Route /api/thoughts/:thoughtId/reactions/:reactionId
+router.route("/:thoughtId/reactions/:reactionID").delete(deleteReaction);
