9e6b1a8eae97bc2f6a69da2a172e0c4e06dcfe9c
Jordan Newhouse <jkimys2@gmail.com>
2024-03-30 11:30:18 -0400

    reaction model update

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,5 +1,5 @@
 // Import files
-const { Schema, model } = require("mongoose");
+const { Schema, Types } = require("mongoose");
 
 // Create Reaction Schema
 const reactionSchema = new Schema({
@@ -7,4 +7,20 @@ const reactionSchema = new Schema({
     type: Schema.Types.ObjectId,
     default:
   },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
 });
+
+// Export reaction schema
+module.exports = reactionSchema;
