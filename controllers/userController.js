7ce76a8ce368947bdbcb6bab180d5a35f0fdc3e2
Jordan Newhouse <jkimys2@gmail.com>
2024-03-30 12:14:07 -0400

    user controller update

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,7 +24,7 @@ module.exports = {
         .populate("friends");
 
       if (!user) {
-        return res.status(404).json({ message: "No student with that ID" });
+        return res.status(404).json({ message: "No user with that ID" });
       }
       res.json(user);
     } catch (err) {
@@ -38,6 +38,36 @@ module.exports = {
     try {
       const user = await User.create(req.body);
       res.json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+
+  // Update a user
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+      if (!user) {
+        return res.status(404).json({ message: "No user with this ID!" });
+      }
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+
+  // Delete a user
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+      if (!user) {
+        return res.status(404).json({ message: "No user with this ID!" });
+      }
     } catch (err) {
       console.log(err);
       return res.status(500).json(err);
