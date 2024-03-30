815eacdacaa2b68fdc66f33603ce5a4fc6f8671f
Jordan Newhouse <jkimys2@gmail.com>
2024-03-30 12:49:32 -0400

    thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -33,7 +33,7 @@ module.exports = {
     try {
       const thought = await Thought.create(req.body);
       const user = await User.findByIdAndUpdate(
-        req.body.userId,
+        { _id: req.body.userId },
         { $addToSet: { thoughts: thought._id } },
         { runValidators: true, new: true }
       );
@@ -72,6 +72,39 @@ module.exports = {
         return res.status(404).json({ message: "No thought with this ID!" });
       }
       return res.status(200).json("Thought has been deleted!");
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+
+  // Post a new reaction
+  async createReaction(req, res) {
+    try {
+      const reaction = await User.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $addToSet: { reactions: req.body } },
+        { runValidators: true, new: true }
+      );
+      return res.status(200).json(reaction);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+
+  // Delete a Reaction
+  async deleteReaction(req, res) {
+    try {
+      const reaction = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $pull: { reactions: { _id: req.params.reactionId } } },
+        { runValidators: true, new: true }
+      );
+      if (!reaction) {
+        return res.status(404).json({ message: "No thought with this ID!" });
+      }
+      return res.status(200).json("Reaction has been deleted!");
     } catch (err) {
       console.log(err);
       return res.status(500).json(err);
