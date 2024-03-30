ba290e2f37b109cf9a1625336a6e42ea29f8ed59
Jordan Newhouse <jkimys2@gmail.com>
2024-03-30 13:07:22 -0400

    api index file update

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,8 +4,8 @@ const thoughtRoutes = require("./thoughtRoutes");
 const userRoutes = require("./userRoutes");
 
 // Set up router
-router.use("thoughts", thoughtRoutes);
-router.use("users", userRoutes);
+router.use("/thoughts", thoughtRoutes);
+router.use("/users", userRoutes);
 
 // Export router
 module.exports = router;
